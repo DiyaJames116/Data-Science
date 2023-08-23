5ffd35798d8856b6fb723ea3fc863c4f419ee5bc
DiyaJames116 <diyaloveschess@gmail.com>
2023-08-23 12:03:47 +0530

    3 fold cross verification

--- DSProject.R
+++ DSProject.R
@@ -24,6 +24,7 @@ print(column_titles)
 #data$Date_of_Joining <- as.Date(data$Date_of_Joining)
 #data$Last_Start_Date <- as.Date(data$Last_Start_Date)
 #data$Last_End_Date <- as.Date(data$Last_End_Date)
+data$Retired <- as.integer(data$Retired)
 
 
 # Data Summaries
@@ -68,28 +69,150 @@ print(cadre_stats)
 
 print(cadre_freq_plot)
 
+#--------
+
+# Create a subset for males and females
+males <- data[data$Gender == "Male", ]
+females <- data[data$Gender == "Female", ]
+
+# Create tables for each gender
+table_males <- table(males$Allotment_Year)
+table_females <- table(females$Allotment_Year)
+
+# Get unique years from both tables
+unique_years <- sort(unique(c(names(table_males), names(table_females))))
+
+# Create a data frame for the result
+result_table <- data.frame(
+  Allotment_Year = unique_years,
+  Male = table_males[match(unique_years, names(table_males))],
+  Female = table_females[match(unique_years, names(table_females))]
+)
+
+print(result_table)
+result_table <- result_table[, !names(result_table) %in% c("Female.Var1", "Male.Var1")]
+print(result_table)
+result_table$Female.Freq[is.na(result_table$Female.Freq)] <- 0
+print(result_table)
+
+#result_table <- result_table[-(1:15), , drop = FALSE]
+result_table <- result_table[-(1:27), , drop = FALSE]
+print(result_table)
+#----------
 
 
-subset_data <- data$Allotment_Year[1:5000]
+#shapiro test for frequency of male
+subset_data <- result_table$Male.Freq
 # Perform Shapiro-Wilk test for normality
 shapiro_test_result <- shapiro.test(subset_data)
 print(shapiro_test_result)
 
-ggplot(data, aes(x = "", y = Allotment_Year)) +
+ggplot(result_table, aes(x = "", y = Male.Freq)) +
  geom_boxplot() +
-labs(title = "Box Plot of Allotment Year", y = "Allotment Year") +
+labs(title = "Box Plot of Male frequency", y = "Male Frequency") +
 theme_minimal()
 
-# Load required library for Kruskal-Wallis test
-library(stats)
+#shapiro test for frequency of female
+subset_data <- result_table$Female.Freq
+# Perform Shapiro-Wilk test for normality
+shapiro_test_result <- shapiro.test(subset_data)
+print(shapiro_test_result)
 
-# Extract the relevant variable for the groups (assuming you have a factor variable)
-groups <- data$Gender[1:5000]  # Replace "GroupColumn" with the actual column name
+ggplot(result_table, aes(x = "", y = Female.Freq)) +
+  geom_boxplot() +
+  labs(title = "Box Plot of Female Frequency", y = "Female Frequency") +
+  theme_minimal()
 
-# Perform Kruskal-Wallis test
-kruskal_test_result <- kruskal.test(subset_data, g = groups)
 
-# Print the test result
-print(kruskal_test_result)
+#kruskal_test_result <- kruskal.test(result_table$Male.Freq, result_table$Female.Freq)
+#print(kruskal_test_result)
+
+#Cov test
+covariance <- cov(result_table$Male.Freq, result_table$Female.Freq)
+print(covariance)
+
+#Correlation test
+correlation <- cor(result_table$Male.Freq, result_table$Female.Freq)
+print(correlation)
+
+#Linear Model test
+model <- lm(Female.Freq ~ Male.Freq, data = result_table)
+# Print the model summary
+summary(model)
+
+#ANOVA test
+one.way <- aov(Female.Freq ~ Male.Freq, data=result_table)
+summary(one.way)
+two.way <- aov(Allotment_Year ~ Female.Freq + Male.Freq, data= result_table)
+summary(two.way)
+
+
+#3-fold cross verification
+# Split the data into three parts
+set.seed(123)  # For reproducibility
+n <- nrow(result_table)
+indices <- sample(1:n, size = n)
+subset_size <- n %/% 3
+
+subset1 <- result_table[indices[1:subset_size], ]
+subset2 <- result_table[indices[(subset_size + 1):(2 * subset_size)], ]
+subset3 <- result_table[indices[(2 * subset_size + 1):n], ]
+
+# Calculate covariance and correlation for each subset
+covariance1 <- cov(subset1$Male.Freq, subset1$Female.Freq)
+correlation1 <- cor(subset1$Male.Freq, subset1$Female.Freq)
+
+covariance2 <- cov(subset2$Male.Freq, subset2$Female.Freq)
+correlation2 <- cor(subset2$Male.Freq, subset2$Female.Freq)
+
+covariance3 <- cov(subset3$Male.Freq, subset3$Female.Freq)
+correlation3 <- cor(subset3$Male.Freq, subset3$Female.Freq)
+
+# Print the results
+cat("Subset 1:\n")
+cat("Covariance:", covariance1, "\n")
+cat("Correlation:", correlation1, "\n\n")
+
+cat("Subset 2:\n")
+cat("Covariance:", covariance2, "\n")
+cat("Correlation:", correlation2, "\n\n")
+
+cat("Subset 3:\n")
+cat("Covariance:", covariance3, "\n")
+cat("Correlation:", correlation3, "\n")
+# Create a data frame for the results
+results <- data.frame(
+  Subset = c("Subset 1", "Subset 2", "Subset 3"),
+  Covariance = c(covariance1, covariance2, covariance3),
+  Correlation = c(correlation1, correlation2, correlation3)
+)
+
+# Print the results data frame
+print(results)
+
+
+
+
+#creating a new table with home and away postings
+filtered_data <- subset(data, Place_of_Domicile != "-" & Place_of_Domicile != "Not Found")
+print(filtered_data[, c("Allotment_Year","Place_of_Domicile", "Cadre")])
+
+#library(dplyr)
+# Create a new data frame with counts for home and away postings
+posting_counts <- filtered_data %>%
+  filter(Place_of_Domicile != "-" & Place_of_Domicile != "Not Found") %>%
+  group_by(Allotment_Year) %>%
+  summarize(
+    Home_Postings = sum(Place_of_Domicile == Cadre),
+    Away_Postings = sum(Place_of_Domicile != Cadre)
+  )
+
+posting_table <- posting_counts[-(1:27), , drop = FALSE]
+# Print the posting counts
+print(posting_table)
+
+
+
+
 
 
